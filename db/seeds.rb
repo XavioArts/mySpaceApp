a6c9964b96ee1b2ee5fad55282bc9df934cad813
Fabio Xavier <fabio.g.xavier33@gmail.com>
2021-12-20 14:55:38 -0700

    seeded sample posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-u1 = User.create(email: "zwarriors@test.com", password: 123456);
+u1 = User.create(email: "x@test.com", password: 123456, name: "Jonny Test", username: "testes15q", )
+
+u1.posts.create(content: "HI this is my first post", likes: 0)
+u1.posts.create(content: "A Race in Space is Dangerous, Baby", likes: 0)
+u1.posts.create(content: "Even vacuum cleaners fall in love, baby", likes: 0)
 
 p u1
+
+p Post.all.length

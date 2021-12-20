bc2453260896b535d13678f6cf5516b95a0658fb
Fabio Xavier <fabio.g.xavier33@gmail.com>
2021-12-20 00:11:43 -0700

    started edit user form

--- client/src/pages/Protected.js
+++ client/src/pages/Protected.js
@@ -1,4 +1,5 @@
-import React, { useContext } from "react";
+import { Box, Button, TextField } from "@mui/material";
+import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../providers/AuthProvider";
 
@@ -6,11 +7,22 @@ const Protected = () => {
 
     const navigate = useNavigate();
     const auth = useContext(AuthContext);
+    const [username, setUsername] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("submitted ", username);
+    };
 
     return (
         <div>
-            <h1>Protected</h1>
-            <button onClick={()=>auth.handleLogout(navigate)} >Log Out</button>
+            <h1>Settings</h1>
+            <p>edit form will go here</p>
+            <Box component="form" autoComplete="off" onSubmit={handleSubmit} >
+                <TextField id="username" label="Username" value={username} onChange={(e)=>setUsername(e.target.value)} />
+                <Button type="submit" >Submit</Button>
+            </Box>
+            <Button>Delete</Button>
         </div>
     );
 };

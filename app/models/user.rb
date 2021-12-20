ea5fa7fe34eb3b6410385c2a499ca1a0138f10af
Fabio Xavier <fabio.g.xavier33@gmail.com>
2021-12-20 14:28:43 -0700

    added posts model and migrations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,32 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
+
+  serialize :liked_posts, Array
+  serialize :friends, Array
+
   extend Devise::Models 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  has_many :posts, dependent: :destroy
+
+  def self.unfriended_users(ids)
+    ids = ids.empty? ? [0] : ids
+    User.where("id NOT IN (?)", ids).order("created_at DESC")
+  end
+
+  def self.friended_users(ids)
+    ids = ids.empty? ? [0] : ids
+    User.where("id IN (?)", ids)
+  end
+
+  def self.liked(ids)
+    ids = ids.empty? ? [0] : ids
+    Post.where("id IN (?)", ids)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_20_050204) do
+ActiveRecord::Schema.define(version: 2021_12_20_205224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.integer "likes"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -40,10 +49,13 @@ ActiveRecord::Schema.define(version: 2021_12_20_050204) do
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
     t.string "username"
+    t.text "liked_posts"
+    t.text "friends"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
